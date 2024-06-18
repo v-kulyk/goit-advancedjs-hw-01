1675583c82dbe4abf2ad9085a6fa6640d9d6a131
Volodymyr Kulyk <vlad.dev.zp@gmail.com>
2024-06-18 20:15:54 +0300

    import SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,6 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
