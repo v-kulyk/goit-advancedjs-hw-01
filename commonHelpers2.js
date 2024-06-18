ccde30a2cbc26daaec4a14a2dbb1311e426673ec
v-kulyk <kulykvladimir@gmail.com>
2024-06-18 18:20:50 +0000

    Deploying to gh-pages from @ v-kulyk/goit-advancedjs-hw-01@153552088c5c32ff78ab82a19b026fcf4b00440e 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{P as o}from"./assets/vendor-1e54b975.js";const e=new o("vimeo-player");e.setCurrentTime(window.localStorage.getItem("videoplayer-current-time")||0);e.on("timeupdate",function(t){window.localStorage.setItem("videoplayer-current-time",t.seconds)});
+//# sourceMappingURL=commonHelpers2.js.map
