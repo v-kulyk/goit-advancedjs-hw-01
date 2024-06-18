b892bc148040d0533e67ee0af90cf8c06a40d10c
Volodymyr Kulyk <vlad.dev.zp@gmail.com>
2024-06-18 20:40:32 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,9 @@
+import Player from '@vimeo/player';
 
+const playerInstance = new Player('vimeo-player');
+
+playerInstance.setCurrentTime(window.localStorage.getItem('videoplayer-current-time') || 0);
+
+playerInstance.on('timeupdate', function (evtData) {
+    window.localStorage.setItem('videoplayer-current-time', evtData.seconds);
+});
